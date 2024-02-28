194c23f1d6b61b216adf7972d17523548dacff3f
suk-6 <me@suk.kr>
2024-02-28 15:49:40 +0900

    Add single signup functionality to tools.py

--- tools.py
+++ tools.py
@@ -3,10 +3,22 @@ from db import userDB
 userDBC = userDB()
 
 
-if __name__ == "__main__":
+def singleSignup():
     id = input("id: ")
     password = input("password: ")
     name = input("name: ")
     admin = 1
 
     userDBC.insert(id, password, name, admin)
+
+
+if __name__ == "__main__":
+    print(
+        """
+        1. Single signup
+        """
+    )
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        singleSignup()
